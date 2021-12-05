c3df7113d21b93d46e1c73871da978f7f1f793be
Deema Burhama <eng.dburhama@gmail.com>
2021-12-05 17:40:13 +0300

    check

---
+++ components/listofappointment/ListOfAppointments.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const ListOfAppointments = () => {
+  return (
+    <View>
+      <Text>Hi</Text>
+    </View>
+  );
+};
+export default ListOfAppointments;
+const styles = StyleSheet.create({});
