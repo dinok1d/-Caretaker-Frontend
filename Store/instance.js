71c71cec8f8a1d74bfe10561a8282ea18abdc564
Deema Burhama <eng.dburhama@gmail.com>
2021-12-18 00:13:10 +0300

    done

--- Store/instance.js
+++ Store/instance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // deema's IP
-// export const baseURL = "http://192.168.8.111:8000";
+export const baseURL = "http://192.168.8.111:8000";
 
 // abdullah's IP
 // export const baseURL = "http://192.168.8.130:8000";
